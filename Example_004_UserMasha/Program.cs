7c4d157d4c948e372065fb397333dec7d331e24c
NeaMary <neamarysm@gmail.com>
2022-10-13 10:33:11 +0300

    hello masha

---
+++ Example_004_UserMasha/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter your name");
+string username = Console.ReadLine();
+if (username.ToLower() == "masha"){
+    Console.WriteLine("Yeeh! This is Masha!!");
+}
+else {
+    Console.Write("Hello, ");
+    Console.Write(username);
+}
