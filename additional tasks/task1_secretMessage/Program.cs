0cd64ae7a9cea05fe31a5d9662548096a10532da
NeaMary <neamarysm@gmail.com>
2022-10-15 17:13:45 +0300

    task1 secretMessage

---
+++ additional tasks/task1_secretMessage/Program.cs
@@ -0,0 +1,27 @@
+﻿string secret = "some secret message";
+string password = "verydifficultpassword";
+int attempts = 3;
+
+while (Convert.ToBoolean(attempts))
+{
+    Console.WriteLine("Enter your password");
+    string input = Console.ReadLine();
+    attempts--;
+
+    if (input == password)
+    {
+        Console.WriteLine(secret);
+        attempts = 0; 
+    }
+    else
+    {
+        if (attempts == 0)
+        {
+            Console.WriteLine("No attempts left");
+        }
+        else
+        {
+            Console.WriteLine("Wrong password. Try again. " + attempts + " attempts left.");
+        }
+    }
+}
