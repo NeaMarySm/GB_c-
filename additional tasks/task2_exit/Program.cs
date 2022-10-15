421764d80443d9dd8c6de2da1b4b0a75676d104a
NeaMary <neamarysm@gmail.com>
2022-10-15 17:24:38 +0300

    task_2 exit word

---
+++ additional tasks/task2_exit/Program.cs
@@ -0,0 +1,10 @@
+﻿string exitWord = "exit";
+bool cycle = true;
+while(cycle){
+    Console.WriteLine("Enter Something");
+    string input = Console.ReadLine();
+    if(input == exitWord){
+        cycle = false;
+    }
+}
+Console.WriteLine("End");
