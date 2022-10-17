ced7fbe640d0362ba2db72c902595bbfdc587f82
NeaMary <neamarysm@gmail.com>
2022-10-18 01:15:43 +0300

    task_13 third digit

---
+++ practice/hw_2/task_13_thirdDigit/Program.cs
@@ -0,0 +1,36 @@
+﻿/* 
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+int initialNumber = ReadNumber("Enter your number ");
+int number = initialNumber;
+bool cycle = true;
+if (number / 100 == 0)
+{
+    Console.WriteLine("The number has less then 3 digits");
+}
+else
+{
+    while (cycle)
+    {
+        if (number / 1000 != 0)
+        {
+            number = number / 10;
+        }
+        else
+        {
+            Console.WriteLine($"Third digit of number {initialNumber} is {number % 10}");
+            cycle = false;
+        }
+    }
+}
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
