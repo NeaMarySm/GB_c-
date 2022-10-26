b72ac45557f6a2d2a9ea8d41c4deceeb89e11ec7
NeaMary <neamarysm@gmail.com>
2022-10-26 09:09:12 +0300

    tasks with cycle for

---
+++ Example_009_CharReplace/Program.cs
@@ -0,0 +1,27 @@
+﻿string text = "В последней своей книге «Научитесь мыслить и рисовать как Леонардо да Винчи: семь шагов к гениальности каждый день» \nМайкл Гелб описывает 7 творческих принципов знаменитого мастера, чтобы развить креативность и максимально \nактивизировать свой творческий потенциал";
+
+string newText = Replace(text, 'а', '@');
+// Console.WriteLine(newText);
+
+newText = Replace(newText, ' ', '_');
+Console.WriteLine(newText);
+
+
+string Replace(string text, char oldChar, char newChar)
+{
+    int length = text.Length;
+    string result = String.Empty;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldChar)
+        {
+            result = result + $"{newChar}";
+        }
+        else
+        {
+            result = result + text[i];
+        }
+    }
+    return result;
+}
+
---
+++ Example_010_ArraySelectSort/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] arr = { 2, 4, 5, 2, 9, 4, 1, 0, 3, 6, 8 };
+Sort(arr);
+PrintArray(arr);
+
+void PrintArray(int[] array)
+{
+    foreach (int elem in array)
+    {
+        Console.Write(elem + " ");
+    }
+}
+
+
+void Sort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        int temp = array[i];
+        array[i] = array[minIndex];
+        array[minIndex] = temp;
+    }
+}
+
