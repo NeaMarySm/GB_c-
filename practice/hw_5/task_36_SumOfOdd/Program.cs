371df53d9234232c63c98c09f8c5b3e967e87dbd
NeaMary <neamarysm@gmail.com>
2022-11-14 11:40:15 +0300

    task36 sum of elements on odd positions

---
+++ practice/hw_5/task_36_SumOfOdd/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+int length = ReadNumber("Enter array length");
+int[] randomArray = GenerateRandomArray(length);
+PrintArray(randomArray);
+int sum = SumOfElemsOnOddPosition(randomArray);
+Console.WriteLine($"Sum of elements on odd positions is {sum}");
+
+int SumOfElemsOnOddPosition(int[] array)
+{
+	int sum = 0;
+	for (int i = 1; i < length; i += 2)
+	{
+		sum += array[i];
+	}
+	return sum;
+}
+
+int[] GenerateRandomArray(int arrayLength)
+{
+	int[] array = new int[arrayLength];
+	for (int i = 0; i < arrayLength; i++)
+	{
+		array[i] = new Random().Next(-99, 100);
+	}
+	return array;
+}
+
+void PrintArray(int[] arr)
+{
+	Console.WriteLine();
+	for (int i = 0; i < arr.Length; i++)
+	{
+		Console.Write(arr[i] + " ");
+	}
+	Console.WriteLine();
+}
+
+int ReadNumber(string message)
+{
+	Console.WriteLine(message);
+	return Convert.ToInt32(Console.ReadLine());
+}
