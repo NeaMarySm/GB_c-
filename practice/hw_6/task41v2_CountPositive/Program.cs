279faa5e1dbffc141ed4b355cf5e79f90aac1cbf
NeaMary <neamarysm@gmail.com>
2022-11-15 12:38:45 +0300

    task41 count positive nums without convertation to int array

---
+++ practice/hw_6/task41v2_CountPositive/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Enter your numbers separated by commas");
+string input = Console.ReadLine();
+int count = CountPositive(input);
+Console.WriteLine(count);
+
+int CountPositive(string str)
+{
+	int notPositive = 0;
+	int length = 0;
+	for (int i = 0; i < str.Length; i++)
+	{
+		if (str[i] == ',')
+		{
+			length++;
+		}
+		if (str[i] == '-' || (str[i] == 0 && str[i + 1] == ','))
+		{
+			notPositive++;
+		}
+	}
+	return length - notPositive;
+}
