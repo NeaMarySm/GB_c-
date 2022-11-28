3f4426bf66bf5b9a72d0ebf75460598eab4098c2
NeaMary <neamarysm@gmail.com>
2022-11-28 14:24:23 +0300

    practice 9 start

---
+++ practice/task65_rangeWithRecursion/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 6, 7, 8"
+*/
+
+int M = ReadNumber("Enter first number");
+int N = ReadNumber("Enter second number");
+NaturalNumber(M, N);
+
+void NaturalNumber(int startNum, int lastNum)
+{
+	if (startNum > lastNum)
+	{
+		int temp = startNum;
+		startNum = lastNum;
+		lastNum = temp;
+	}
+	if (lastNum == startNum)
+	{
+		Console.Write(startNum + " ");
+	}
+	else
+	{
+		NaturalNumber(startNum, lastNum - 1);
+		Console.Write(lastNum + " ");
+	}
+}
+int ReadNumber(string message)
+{
+	Console.WriteLine(message);
+	return Convert.ToInt32(Console.ReadLine());
+}
---
+++ practice/task67_sumOfDigitsRecurs/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+
+int number = ReadNumber("Enter number");
+
+int Sum(int num)
+{
+	if (num == 0)
+	{
+		return 0;
+	}
+	else
+	{
+		num = Sum(num % 10);
+	}
+	return sum;
+
+}
+
+int ReadNumber(string message)
+{
+	Console.WriteLine(message);
+	return Convert.ToInt32(Console.ReadLine());
+}
