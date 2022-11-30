a212d897c9ad3b23144e527d8008458a37cc0730
NeaMary <neamarysm@gmail.com>
2022-11-30 11:48:32 +0300

    range with recursion

---
+++ practice/hw_9/task64_RangeRecursive/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+int N = ReadNumber("Enter your number:");
+PrintNaturalNumbers(N);
+
+void PrintNaturalNumbers(int start)
+{
+	if (start < 1)
+	{
+		Console.WriteLine("Number should be positive");
+		return;
+	}
+	else if (start == 1)
+		Console.WriteLine(start);
+	else
+	{
+		Console.Write(start + " ");
+		PrintNaturalNumbers(start - 1);
+	}
+}
+
+int ReadNumber(string message)
+{
+	Console.WriteLine(message);
+	return Convert.ToInt32(Console.ReadLine());
+}
